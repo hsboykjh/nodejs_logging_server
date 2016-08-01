b731538eb4a4faf30b24b4cf18da75bf1b485329
hsboykjh <hsboykjh@gmail.com>
2016-08-01 21:46:36 +1000

    Create node_server.js

---
+++ src/node_server.js
@@ -0,0 +1,30 @@
+var net = require('net');
+var fs = require('fs');
+var dateFormat = require('dateformat');
+
+var server = net.createServer(function (connect)
+{ //'connection' listener
+	
+	var now = new Date();
+	var milisecond = now.getTime();
+	var day = dateFormat(now.request_date, "yyyymmdd-hhMMss-"+milisecond);
+	
+	//console.log("day : " + day);
+	var writeStream = fs.createWriteStream(__dirname + "/" + day + ".txt");
+	
+	//console.log('Java client connected to this nodeServer');
+	connect.on('data', function (data)
+    {
+        //console.log(data);   
+        writeStream.write(data);
+    });
+	connect.on('end', function ()
+    {
+        //console.log('nodeServer disconnected');
+        writeStream.end();
+    });
+});
+server.listen(6000, function ()
+{ //'listening' listener
+    console.log('nodeServer listening port:6000');
+});
